63841c23d0cb48854813c97966977a2c1c51b0bd
mrp-v2 <matthrewp@gmail.com>
2020-06-29 07:51:39 -0400

    added config class to registry

--- src/main/java/com/mrp_v2/redstonequit/RedstoneQuit.java
+++ src/main/java/com/mrp_v2/redstonequit/RedstoneQuit.java
@@ -1,9 +1,17 @@
 package com.mrp_v2.redstonequit;
 
+import com.mrp_v2.redstonequit.config.ConfigOptions;
+
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(RedstoneQuit.MODID)
 public class RedstoneQuit {
 
 	public static final String MODID = "redstonequit";
+
+	public RedstoneQuit() {
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ConfigOptions.serverSpec);
+	}
 }
