9ae641896b9b3d391552019a14b7f43f0aca0ebe
mrp-v2 <matthrewp@gmail.com>
2020-06-29 09:56:59 -0400

    added item to registry

--- src/main/java/com/mrp_v2/redstonequit/registry/RegistryHandler.java
+++ src/main/java/com/mrp_v2/redstonequit/registry/RegistryHandler.java
@@ -4,24 +4,33 @@ import com.mrp_v2.redstonequit.RedstoneQuit;
 import com.mrp_v2.redstonequit.block.RedstoneQuitBlock;
 
 import net.minecraft.block.Block;
-//import net.minecraft.item.BlockItem;
-//import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.registries.ObjectHolder;
 
-@EventBusSubscriber(modid = RedstoneQuit.MODID)
+@EventBusSubscriber(modid = RedstoneQuit.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class RegistryHandler {
-	
-	private static final Block redstoneQuitBlock = new RedstoneQuitBlock();
+
+	@ObjectHolder(RedstoneQuit.MODID + ":" + RedstoneQuitBlock.ID)
+	public static final Block redstoneQuitBlock = null;
+	@ObjectHolder(RedstoneQuit.MODID + ":" + RedstoneQuitBlock.ID)
+	public static final Item redstoneQuitBlockItem = null;
 
 	@SubscribeEvent
 	public static void registerBlocks(RegistryEvent.Register<Block> event) {
-		event.getRegistry().register(redstoneQuitBlock);
+		event.getRegistry().register(new RedstoneQuitBlock());
 	}
 
-	/*@SubscribeEvent
+	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
-		event.getRegistry().register(new BlockItem(redstoneQuitBlock).setRegistryName(redstoneQuitBlock.getRegistryName()));
-	}*/
+		event.getRegistry()
+				.register(new BlockItem(redstoneQuitBlock,
+						new Item.Properties().addToolType(ToolType.PICKAXE, 1).group(ItemGroup.REDSTONE))
+								.setRegistryName(RedstoneQuit.MODID, RedstoneQuitBlock.ID));
+	}
 }
