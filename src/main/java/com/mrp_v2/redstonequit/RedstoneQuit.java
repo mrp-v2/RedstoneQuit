da278fdc85a17416cc73c9b48569c2a982f0b0c1
mrp-v2 <matthrewp@gmail.com>
2020-06-29 07:27:21 -0400

    create main mod class

---
+++ src/main/java/com/mrp_v2/redstonequit/RedstoneQuit.java
@@ -0,0 +1,9 @@
+package com.mrp_v2.redstonequit;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RedstoneQuit.MODID)
+public class RedstoneQuit {
+
+	public static final String MODID = "redstonequit";
+}
