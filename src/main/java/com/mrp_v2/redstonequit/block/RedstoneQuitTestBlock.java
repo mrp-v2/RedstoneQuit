da7b794a661285e45d260c1eb118b106b7178da2
mrp-v2 <matthrewp@gmail.com>
2020-07-01 08:10:45 -0400

    implement new method

--- src/main/java/com/mrp_v2/redstonequit/block/RedstoneQuitTestBlock.java
+++ src/main/java/com/mrp_v2/redstonequit/block/RedstoneQuitTestBlock.java
@@ -1,16 +1,23 @@
 package com.mrp_v2.redstonequit.block;
 
+import com.mrp_v2.redstonequit.util.MessageHelper;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.server.ServerWorld;
+
 public class RedstoneQuitTestBlock extends RedstoneQuitBlockBase {
 
 	public static final String ID = "redstone_quit_test_block";
+
+	public static final String testQuitMessage = "(Test Quit Block) You have been kicked!";
 
 	public RedstoneQuitTestBlock() {
 		super(ID);
 	}
 
 	@Override
-	protected void blockPowered(int redstonePower) {
-		// TODO Auto-generated method stub
-
+	void doPlayerAction(PlayerEntity player, ServerWorld worldIn, BlockPos pos) {
+		MessageHelper.SendMessage(player, testQuitMessage);
 	}
 }
