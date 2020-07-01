b63b7662045585124d754763ccf91b463b4f6e0b
mrp-v2 <matthrewp@gmail.com>
2020-07-01 17:04:25 -0400

    added config class to event bus

--- src/main/java/com/mrp_v2/redstonequit/RedstoneQuit.java
+++ src/main/java/com/mrp_v2/redstonequit/RedstoneQuit.java
@@ -1,10 +1,12 @@
 package com.mrp_v2.redstonequit;
 
-import com.mrp_v2.redstonequit.config.ConfigOptions;
+import com.mrp_v2.redstonequit.config.RedstoneQuitConfig;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(RedstoneQuit.MODID)
 public class RedstoneQuit {
@@ -13,6 +15,8 @@ public class RedstoneQuit {
 	public static final String TRANSLATION_KEY_STEM = "mrp_v2." + MODID + ".";
 
 	public RedstoneQuit() {
-		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ConfigOptions.serverSpec);
+		ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, RedstoneQuitConfig.serverSpec);
+		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.register(RedstoneQuitConfig.class);
 	}
 }
