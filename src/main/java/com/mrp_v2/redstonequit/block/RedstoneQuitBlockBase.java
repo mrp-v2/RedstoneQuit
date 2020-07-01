a9e13ad4c82522c9636cf1fc3645a4c9f847af27
mrp-v2 <matthrewp@gmail.com>
2020-07-01 08:10:22 -0400

    overide neighborChanged, add abstract doPlayerAction for subclasses

--- src/main/java/com/mrp_v2/redstonequit/block/RedstoneQuitBlockBase.java
+++ src/main/java/com/mrp_v2/redstonequit/block/RedstoneQuitBlockBase.java
@@ -1,17 +1,21 @@
 package com.mrp_v2.redstonequit.block;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import com.mrp_v2.redstonequit.RedstoneQuit;
+import com.mrp_v2.redstonequit.config.ConfigOptions;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.ToolType;
 
@@ -25,9 +29,17 @@ abstract public class RedstoneQuitBlockBase extends Block {
 	}
 
 	@Override
+	public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos,
+			boolean isMoving) {
+		if (!worldIn.isRemote) {
+			worldIn.getPendingBlockTicks().scheduleTick(pos, this, 0);
+		}
+	}
+
+	@Override
 	public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
 		if (worldIn.isBlockPowered(pos)) {
-			blockPowered(worldIn.getRedstonePowerFromNeighbors(pos));
+			blockPowered(worldIn.getRedstonePowerFromNeighbors(pos), worldIn, pos);
 		}
 	}
 
@@ -36,5 +48,48 @@ abstract public class RedstoneQuitBlockBase extends Block {
 				.setRegistryName(this.getRegistryName());
 	}
 
-	abstract protected void blockPowered(int redstonePower);
+	// Even numbers are all,
+	// Odds are nearest
+	protected void blockPowered(int redstonePower, ServerWorld worldIn, BlockPos pos) {
+		if (redstonePower <= 0 || redstonePower >= 15) {
+			return;
+		}
+		int range = ConfigOptions.getRangeForStrength(redstonePower);
+		if (redstonePower % 2 == 0) {
+			for (PlayerEntity pe2 : worldIn.getPlayers()) {
+				if (playerWithinRange(pos, pe2, range)) {
+					doPlayerAction(pe2, worldIn, pos);
+				}
+			}
+		} else {
+			PlayerEntity pe1 = worldIn.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), range, null);
+			if (pe1 != null) {
+				if (playerWithinRange(pos, pe1, range)) {
+					doPlayerAction(pe1, worldIn, pos);
+				}
+			}
+		}
+	}
+
+	abstract void doPlayerAction(PlayerEntity player, ServerWorld worldIn, BlockPos pos);
+
+	protected ArrayList<PlayerEntity> getNearbyPlayers(ServerWorld worldIn, BlockPos pos, double range) {
+		ArrayList<PlayerEntity> nearbyPlayers = new ArrayList<PlayerEntity>();
+		for (PlayerEntity pe : worldIn.getPlayers()) {
+			if (playerWithinRange(pos, pe, range)) {
+				nearbyPlayers.add(pe);
+			}
+		}
+		return nearbyPlayers;
+	}
+
+	protected boolean playerWithinRange(BlockPos pos, PlayerEntity player, double range) {
+		if (range < 0) {
+			return true;
+		}
+		if (range == 0) {
+			return false;
+		}
+		return pos.withinDistance(player.getPositionVec(), range);
+	}
 }
